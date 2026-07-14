3b956aa73c07983cbdd20b6cb459b43f5ce82e51
tial0011 <taiwoalex34@gmail.comgit config --global user.name tial0011git config --global user.email taiwoalex34@gmail.com>
2026-07-14 02:40:51 +0100

    okay

--- js/firebase/config.js
+++ js/firebase/config.js
@@ -1,7 +1,10 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-
+import {
+  getAnalytics,
+  isSupported,
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-analytics.js";
 // Your config
 const firebaseConfig = {
   apiKey: "AIzaSyBzKtJ5P_2bL0LWDOJt4KqMlA1Z8sMOSwo",
@@ -17,6 +20,15 @@ const firebaseConfig = {
 // Initialize
 const app = initializeApp(firebaseConfig);
 
-// 🔥 EXPORT THESE
-export const auth = getAuth(app);
+// 🔥 EXPORT THESEexport const auth = getAuth(app);
 export const db = getFirestore(app);
+
+let analytics = null;
+
+isSupported().then((supported) => {
+  if (supported) {
+    analytics = getAnalytics(app);
+  }
+});
+
+export { analytics };
